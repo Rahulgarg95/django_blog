c262b5ef5e99326d98848d76aa931d6c7f8806b7
GARG <rahul.garg3@hpe.com>
2020-01-11 14:05:04 +0530

    Basic Templating Done

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts = [
+    {
+        'author' : 'Rahul Garg',
+        'title' : 'Blog Post 1',
+        'content' : 'JNU protest issue rising every day',
+        'date_posted' : 'Jan 11 2020'
+    },
+    {
+        'author' : 'Rohit Agarwal',
+        'title' : 'Blog Post 2',
+        'content' : 'Amit Shah imposes CAA',
+        'date_posted' : 'Jan 05 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog - Rahul</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1> Blog About - Rahul </h2>')
+    return render(request, 'blog/about.html', {'title': 'About!!!'})
