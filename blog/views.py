e5cf46a0560072f222daa6d7116ff33454bd196b
GARG <rahul.garg3@hpe.com>
2020-01-12 11:46:54 +0530

    User Registeration and Login Done

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 #from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Rahul Garg',
-        'title' : 'Blog Post 1',
-        'content' : 'JNU protest issue rising every day',
-        'date_posted' : 'Jan 11 2020'
-    },
-    {
-        'author' : 'Rohit Agarwal',
-        'title' : 'Blog Post 2',
-        'content' : 'Amit Shah imposes CAA',
-        'date_posted' : 'Jan 05 2020'
-    }
-]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
